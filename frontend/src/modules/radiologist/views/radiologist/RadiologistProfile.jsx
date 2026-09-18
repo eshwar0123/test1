a615446559bd3d643073e6d731b9a3e344892ac6
Ubuntu <ubuntu@ip-172-31-6-192.ap-south-1.compute.internal>
2026-09-18 12:38:59 +0000

    Remove frontend and backend from repository

--- frontend/src/modules/radiologist/views/radiologist/RadiologistProfile.jsx
+++
@@ -1,3 +0,0 @@
-export default function RadiologistProfile() {
-  return <div className="p-4">Radiologist Profile Page</div>;
-}
--- frontend/src/routes.js
+++
@@ -1,8 +0,0 @@
-import Dashboard from "./views/dashboard/Dashboard";
-
-const routes = [
-  { path: "", element: <Dashboard /> },
-  { path: "dashboard", element: <Dashboard /> },
-];
-
-export default routes;
